28addadd502aed9e913fae50adebe0ba096fc30d
benjaminbartels <bbartels@gmail.com>
2017-09-22 12:03:53 -0700

    modify converage.sh

--- coverage.sh
+++ coverage.sh
@@ -7,7 +7,6 @@
 # Usage: script/coverage [--html|--coveralls]
 #
 #     --html      Additionally create HTML report and open it in browser
-#     --coveralls Push coverage statistics to coveralls.io
 #
 
 set -e
@@ -20,7 +19,7 @@ generate_cover_data() {
     rm -rf "$workdir"
     mkdir "$workdir"
 
-    for pkg in $(go list ./... | grep -v /vendor/); do
+    for pkg in "$@"; do
         f="$workdir/$(echo $pkg | tr / -).cover"
         go test -v -covermode="$mode" -coverprofile="$f" "$pkg"
     done
@@ -33,20 +32,13 @@ show_cover_report() {
     go tool cover -${1}="$profile"
 }
 
-push_to_coveralls() {
-    echo "Pushing coverage statistics to coveralls.io"
-    goveralls -coverprofile="$profile"
-}
-
-generate_cover_data $(go list ./...)
+generate_cover_data $(go list ./... | grep -v /vendor/)
 show_cover_report func
 case "$1" in
 "")
     ;;
 --html)
     show_cover_report html ;;
---coveralls)
-    push_to_coveralls ;;
 *)
     echo >&2 "error: invalid option: $1"; exit 1 ;;
 esac
