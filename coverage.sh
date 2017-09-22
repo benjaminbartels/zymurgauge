6021cb906e27acc084030bb25c8b326eddb129d0
benjaminbartels <bbartels@gmail.com>
2017-09-22 14:29:12 -0700

    edit coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -15,24 +15,22 @@ workdir=.cover
 profile="c.out"
 mode=count
 
-generate_cover_data() {
-    rm -rf "$workdir"
-    mkdir "$workdir"
-
-    for pkg in "$@"; do
-        f="$workdir/$(echo "$pkg" | tr / -).cover"
-        go test -v -covermode="$mode" -coverprofile="$f" "$pkg"
-    done
-
-    echo "mode: $mode" >"$profile"
-    grep -h -v "^mode:" "$workdir"/*.cover >>"$profile"
-}
-
 show_cover_report() {
-    go tool cover -"${1}"="$profile"
+    go tool cover -${1}="$profile"
 }
 
-generate_cover_data $(go list ./... | grep -v /vendor/)
+rm -rf "$workdir"
+mkdir "$workdir"
+
+for pkg in $(go list ./... | grep -v /vendor/); do
+    f="$workdir/$(echo $pkg | tr / -).cover"
+    go test -v -covermode="$mode" -coverprofile="$f" "$pkg"
+done
+
+echo "mode: $mode" >"$profile"
+grep -h -v "^mode:" "$workdir"/*.cover >>"$profile"
+
+
 show_cover_report func
 case "$1" in
 "")
