390566a03086dabcd8ec1900348cab28370a0a28
benjaminbartels <bbartels@gmail.com>
2018-08-28 11:30:46 -0600

    purposelybreak build

--- cmd/zymsrvd/ui/config/prod.env.js
+++ cmd/zymsrvd/ui/config/prod.env.js
@@ -4,6 +4,6 @@ module.exports = {
   API_URL: '"https://www.zymurgauge.com/api/v1/"',
   AUTH_DOMAIN: '"zymurgauge.auth0.com"',
   AUTH_CLIENT_ID: '"l7xOvvCNf0vSnhJG2W42vvJFytGv7hPc"',
-  AUTH_CALLBACK_URL: '"https://www.zymurgauge.com/auth"',
+  AUTH_CALLBACK_URL_BREAK_BUILD: '"https://www.zymurgauge.com/auth"',
   AUTH_IDENTIFIER: '"https://www.zymurgauge.com/api"'
 }
