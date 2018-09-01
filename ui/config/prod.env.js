a34609146fff6b788e00c722ccf833bf473b5e75
benjaminbartels <bbartels@gmail.com>
2018-09-01 08:32:28 -0600

    temporarily changeing url

--- ui/config/prod.env.js
+++ ui/config/prod.env.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://www.zymurgauge.com/api/v1/"',
+  API_URL: '"https://www.all4days.com/api/v1/"',
   AUTH_DOMAIN: '"zymurgauge.auth0.com"',
   AUTH_CLIENT_ID: '"lM0z534IAwIPHJesf00IKH1f1oZOYwOH"',
-  AUTH_CALLBACK_URL: '"https://www.zymurgauge.com/auth"',
+  AUTH_CALLBACK_URL: '"https://www.all4days.com/auth"',
   AUTH_AUDIENCE: '"zymurgauge.com/api"'
 }
