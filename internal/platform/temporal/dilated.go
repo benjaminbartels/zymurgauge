1cf8cf1b597a7ea9c56959d2cdf511445404514a
benjaminbartels <bbartels@gmail.com>
2019-06-18 11:02:13 -0600

    rename clock multiplyer to speed

--- internal/platform/temporal/dilated.go
+++ internal/platform/temporal/dilated.go
@@ -5,23 +5,23 @@ import (
 )
 
 type DilatedClock struct {
-	multiplyer float64
-	startTime  time.Time
+	speed     float64
+	startTime time.Time
 }
 
-func NewDilatedClock(multiplyer float64) Clock {
+func NewDilatedClock(speed float64) Clock {
 	return &DilatedClock{
-		multiplyer: multiplyer,
-		startTime:  time.Now(),
+		speed:     speed,
+		startTime: time.Now(),
 	}
 }
 
 func (dc *DilatedClock) Now() time.Time {
 	diff := float64(time.Since(dc.startTime)) / float64(time.Nanosecond)
-	return dc.startTime.Add(time.Duration(dc.multiplyer * diff))
+	return dc.startTime.Add(time.Duration(dc.speed * diff))
 }
 
 func (dc *DilatedClock) After(d time.Duration) <-chan time.Time {
-	d = d / time.Duration(dc.multiplyer)
+	d = d / time.Duration(dc.speed)
 	return time.After(d)
 }
