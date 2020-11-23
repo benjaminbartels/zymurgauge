61e73e0d3d2ff2318412ca6f447489b885ee5103
Benjamin Bartels <bbartels@gmail.com>
2020-11-22 17:35:30 -0700

    Sim unit tests (#17)
    * simulator unit tests

---
+++ cmd/zymsim/simulator/simulator_test.go
@@ -0,0 +1,83 @@
+package simulator_test
+
+import (
+	"testing"
+
+	"github.com/benjaminbartels/zymurgauge/cmd/zymsim/simulator"
+)
+
+const initialBeerTemp = 25.0
+
+func TestChill(t *testing.T) {
+	expected := 24.556124476072753
+	sim := simulator.New(initialBeerTemp)
+
+	if err := sim.Chiller.On(); err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		sim.Update()
+	}
+
+	temp, err := sim.Thermometer.Read()
+	if err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	if expected != temp {
+		t.Errorf("Unexpected temp. Want: '%f', Got: '%f'", expected, temp)
+	}
+}
+
+func TestHeat(t *testing.T) {
+	expected := 25.771302060855625
+	sim := simulator.New(initialBeerTemp)
+
+	if err := sim.Heater.On(); err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		sim.Update()
+	}
+
+	temp, err := sim.Thermometer.Read()
+	if err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	if expected != temp {
+		t.Errorf("Unexpected temp. Want: '%f', Got: '%f'", expected, temp)
+	}
+}
+
+func TestOnOf(t *testing.T) {
+	expected := 23.964487886702734
+	sim := simulator.New(initialBeerTemp)
+
+	if err := sim.Chiller.On(); err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		sim.Update()
+	}
+
+	if err := sim.Chiller.Off(); err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		sim.Update()
+	}
+
+	temp, err := sim.Thermometer.Read()
+	if err != nil {
+		t.Errorf("Unexpected error. Got: %+v", err)
+	}
+
+	if expected != temp {
+		t.Errorf("Unexpected temp. Want: '%f', Got: '%f'", expected, temp)
+	}
+}
