3a552fc5b72afe06125f93a145bf8616ad25bfe2
Benjamin Bartels <bbartels@gmail.com>
2020-11-25 17:55:51 -0700

    remove unused code (#19)

--- internal/storage/thermostat.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-type Thermostat struct {
-	ThermometerID string `json:"thermometerId"`
-	ChillerPin    string `json:"chillerPin,omitempty"`
-	HeaterPin     string `json:"heaterPin,omitempty"`
-}
