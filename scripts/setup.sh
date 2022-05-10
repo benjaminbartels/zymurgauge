bc3090d147877f40b7ba07a92729ddc25ca3cc93
benjaminbartels <bbartels@gmail.com>
2022-05-10 10:23:59 -0600

    remove auth and enable flex on startup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,3 @@
-
-PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
 ZYM_PATH=$HOME/.zymurgauge
 
 # create directories 
@@ -11,9 +9,15 @@ mkdir -p ${ZYM_PATH}/telegraf
 # copy conf files that are to be updated to their respective data directories
 cp config/nginx.conf ${ZYM_PATH}/nginx
 cp config/influxdb.conf ${ZYM_PATH}/influxdb
-cp config/create-telegraf.iql ${ZYM_PATH}/influxdb/init
+cp config/init.iql ${ZYM_PATH}/influxdb/init
 cp config/telegraf.conf ${ZYM_PATH}/telegraf
 
-# update conf files with generated password
-sed -i 's/^  # password = "metricsmetricsmetricsmetrics"$/  password = "'${PASSWORD}'"/' ${ZYM_PATH}/telegraf/telegraf.conf
-sed -i 's/metricsmetricsmetricsmetrics/'$PASSWORD'/g' ${ZYM_PATH}/influxdb/init/create-telegraf.iql
+# initialze influxdb
+docker run --rm \
+    -v $ZYM_PATH/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
+    -v $ZYM_PATH/influxdb/data:/var/lib/influxdb \
+    -v $ZYM_PATH/influxdb/init:/docker-entrypoint-initdb.d \
+    arm32v7/influxdb:1.8.10 /init-influxdb.sh
+
+# enable flex
+sed -i 's/^  flux-enabled = false$/    flux-enabled = true/' ${ZYM_PATH}/influxdb/influxdb.conf
