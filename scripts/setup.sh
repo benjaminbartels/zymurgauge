4818e6e90ee3e05800adef4cf7d2239de21c737e
benjaminbartels <bbartels@gmail.com>
2022-05-18 11:13:29 -0600

    work in progress

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,23 +1,17 @@
 ZYM_PATH=$HOME/.zymurgauge
 
+# TODO: run zym init with user and pass
+# TODO: dont run containers as root
+
 # create directories 
+mkdir -p ${ZYM_PATH}/data
 mkdir -p ${ZYM_PATH}/nginx
 mkdir -p ${ZYM_PATH}/influxdb/data
+mkdir -p ${ZYM_PATH}/influxdb/config
 mkdir -p ${ZYM_PATH}/influxdb/init
 mkdir -p ${ZYM_PATH}/telegraf
 
 # copy conf files that are to be updated to their respective data directories
 cp config/nginx.conf ${ZYM_PATH}/nginx
-cp config/influxdb.conf ${ZYM_PATH}/influxdb
-cp config/init.iql ${ZYM_PATH}/influxdb/init
+cp config/setup-v1.sh ${ZYM_PATH}/influxdb/init
 cp config/telegraf.conf ${ZYM_PATH}/telegraf
-
-# initialze influxdb
-docker run --rm \
-    -v $ZYM_PATH/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
-    -v $ZYM_PATH/influxdb/data:/var/lib/influxdb \
-    -v $ZYM_PATH/influxdb/init:/docker-entrypoint-initdb.d \
-    arm32v7/influxdb:1.8.10 /init-influxdb.sh
-
-# enable flex
-sed -i 's/^  flux-enabled = false$/    flux-enabled = true/' ${ZYM_PATH}/influxdb/influxdb.conf
