a88fcbe75bcd650d0deeb49ef713ded66b59a4e3
benjaminbartels <bbartels@gmail.com>
2022-05-27 17:12:25 -0600

    setup work

--- config/setup-v1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-influx v1 dbrp create \
-  --bucket-id ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
-  --db ${V1_DB_NAME} \
-  --rp ${V1_RP_NAME} \
-  --default \
-  --org ${DOCKER_INFLUXDB_INIT_ORG}
