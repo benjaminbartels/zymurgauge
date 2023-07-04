d381ce0d7df30b80fe4a1c3d73319f53a987ee02
benjaminbartels <bbartels@gmail.com>
2023-07-04 14:05:41 -0600

    revert contained ctx code

--- internal/chamber/interfaces.go
+++ internal/chamber/interfaces.go
@@ -1,15 +1,13 @@
 package chamber
 
 import (
-	"context"
-
 	"github.com/benjaminbartels/zymurgauge/internal/device"
 	"github.com/benjaminbartels/zymurgauge/internal/device/tilt"
 )
 
 type Controller interface {
 	Repo
-	StartFermentation(ctx context.Context, chamberID string, step string) error
+	StartFermentation(hamberID string, step string) error
 	StopFermentation(chamberID string) error
 }
 
